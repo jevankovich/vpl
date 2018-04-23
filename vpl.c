0d3b586d0cbd324a8941c31b832b127512ec34d7
Justin Evankovich <justinevankovich@gmail.com>
2018-04-23 12:12:19 -0400

    Created readline function. Somehow works

---
+++ vpl.c
@@ -0,0 +1,63 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include <unistd.h>
+
+#define DEFAULT_BUF 1
+size_t readline(char **buf, size_t *len, size_t *readlen, size_t linelen) {
+	if (*buf == NULL || *len == 0) {
+		*buf = malloc(DEFAULT_BUF);
+		*len = DEFAULT_BUF;
+	}
+
+	if (*readlen > linelen) {
+		memmove(*buf, *buf + linelen, *readlen - linelen);
+	}
+
+	*readlen = *readlen - linelen;
+
+	linelen = 0;
+	while (1) {
+		for (; linelen < *readlen; linelen++) {
+			if ((*buf)[linelen] == '\n') {
+				goto newline;
+			}
+		}
+
+		if (*readlen == *len) {
+			*buf = realloc(*buf, (*len) * 2);
+			*len *= 2;
+		}
+
+		*readlen += read(0, *buf + *readlen, *len - *readlen);
+	}
+
+newline:
+	linelen++; // Count the \n too
+	return linelen;
+}
+
+const char *test = "hi there\na\n";
+int main() {
+	char *buf = NULL;
+	size_t buflen = 0;
+	size_t readlen = 0;
+	size_t linelen = 0;
+
+	/*
+	buf = malloc(512);
+	strcpy(buf, test);
+	buflen = 512;
+	readlen = strlen(test);
+	linelen = 0;
+	*/
+
+	while (1) {
+		write(1, "> ", 2);
+		linelen = readline(&buf, &buflen, &readlen, linelen);
+		write(1, buf, linelen);
+		if (linelen <= 1) {
+			break;
+		}
+	}
+}
